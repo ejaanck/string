5a61d0671f7e51cbae4a98c9a1814299659c5a37
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-11-10 12:46:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", "1948147616"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SiArab_Support")
+MUST_JOIN = getenv("MUST_JOIN", "SiArab_Support")
