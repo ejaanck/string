0c59dacdbe1d8779ddac936b19c8adf3a6acb39d
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-11-10 12:57:11 +0700

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="ʙᴜᴀᴛ sᴛʀɪɴɢ", callback_data="gensession")],
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="sᴛᴏʀᴇ", url="https://t.me/Arabc0de/2"
             ),
         ],
     ]
