a897b8a11fe74630247a90f283d947dccf0fcc50
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-17 12:58:13 +0700

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -5,7 +5,7 @@ from StringGen import Anony
 from StringGen.utils import add_served_user, keyboard
 
 
-@Anony.on_message(filters.command("start") & filters.private & filters.incoming)
+@Anony.on_message(filters.command("astart") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
         text=f"n",
@@ -13,10 +13,11 @@ async def f_start(_, message: Message):
     )
     await add_served_user(message.from_user.id)
 
-@Anony.on_message(filters.command("start") & filters.private & filters.incoming)
+@Anony.on_message(filters.command("genstring") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"n",
+        text=f"Hai {message.from_user.first_name},\n\nSilahkan Klik Tombol Di Bawah Untuk Mengambil String Session Anda",
+        reply_markup=keyboard,
         disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
