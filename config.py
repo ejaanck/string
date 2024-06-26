9bf59b2ec7fb3a1450005976406a4cbb4b47444c
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-06-27 07:57:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23770828"))
+API_HASH = getenv("API_HASH", "2d3e87f244740e5c8286591940e24cd4")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+BOT_TOKEN = getenv("BOT_TOKEN"," 7009082569:AAHwZgUZv1eN1W04N7-CeNzlQHhYjLScYes")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://sorybang:1234@cluster0.ww8a9ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", "1996421781"))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/jxsupport")
-MUST_JOIN = getenv("MUST_JOIN", "stayheresay")
+OWNER_ID = int(getenv("OWNER_ID", "7149602071"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mchimacha")
+MUST_JOIN = getenv("MUST_JOIN", "thebrazzernew")
