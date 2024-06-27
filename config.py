50db1e3070987ca62d18738d1a85509d69d37004
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-06-27 08:01:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "23770828"))
 API_HASH = getenv("API_HASH", "2d3e87f244740e5c8286591940e24cd4")
 
-BOT_TOKEN = getenv("BOT_TOKEN"," 7009082569:AAHwZgUZv1eN1W04N7-CeNzlQHhYjLScYes")
+BOT_TOKEN = getenv("BOT_TOKEN", "7009082569:AAGtzM7zw0J993cLdxuAFEJaFbreYLf_Kvg")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://sorybang:1234@cluster0.ww8a9ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID", "7149602071"))
